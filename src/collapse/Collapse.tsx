13984eb99fdb223bb399490372b97c52d66064c0
zawette <zawette@gmail.com>
2020-07-24 20:49:12 +0100

    why

--- src/collapse/Collapse.tsx
+++ src/collapse/Collapse.tsx
@@ -1,38 +1,46 @@
-import React, { ReactElement, useState, useRef,useLayoutEffect } from 'react';
+import React, { ReactElement, useState, useRef, useLayoutEffect } from "react";
 import styles from "./Collapse.module.scss";
 
-interface Props {
-    
-}
-
+interface Props {}
 
 export default function Collapse({}: Props): ReactElement {
-    const [expand, setexpand] = useState(true);
-    const [contentHeight, setcontentHeight] = useState(0)
-    const setContentDivHeight= (height:number)=>{
-        if(contentRef.current) contentRef.current.style.height = ""+ height
-    }
-    const contentRef = useRef<HTMLDivElement>(null);
-    const tempFunc= ()=>{
-         expand ?  setContentDivHeight(contentHeight): setContentDivHeight(0)
-        setexpand(!expand)
-    }
+    // todo: contentHeight no need for useState
+  const [expand, setexpand] = useState(true);
+  const [contentHeight, setcontentHeight] = useState(0);
+  const setContentDivHeight = (height: number) => {
+    if (contentRef.current) contentRef.current.style.height = "" + height;
+  };
+  const contentRef = useRef<HTMLDivElement>(null);
+  const tempFunc = (ex:boolean) => {
+    ex ? setContentDivHeight(contentHeight) : setContentDivHeight(0);
+    debugger
 
-    useLayoutEffect(()=>{
+    setexpand(ex);
+  };
+
+  useLayoutEffect(() => {
     setcontentHeight(contentRef.current?.clientHeight || 0);
-return ()=>{setexpand(false);setContentDivHeight(0)}
-},[])
+    setContentDivHeight(0);
+    return setexpand(false);
+  }, []);
 
-    return (
-        <div className={styles.zawCollapse}>
-            <div className="top">
-                temp stuff
-                <button className="expandBtn" onClick={()=> tempFunc()}>^</button>
-            </div>
+  return (
+    <div className={styles.zawCollapse}>
+      <div className="top">
+        temp stuff
+        <button className="expandBtn" onClick={() => tempFunc(!expand)}>
+          ^
+        </button>
+      </div>
 
-            <div className="content" ref={contentRef}>
-                Proident aute elit eiusmod eu consequat. Nulla mollit fugiat qui incididunt aliquip aliqua eu occaecat ea et incididunt. Non ex est consequat do aliqua aliqua ipsum aute commodo eu consequat. Commodo dolor aliqua nostrud qui aliquip tempor magna nulla non voluptate officia eiusmod. Ex Lorem mollit officia labore ut nisi occaecat anim cillum eiusmod mollit et laboris sint.
-            </div>
-        </div>
-    )
+      <div className="content" ref={contentRef}>
+        Proident aute elit eiusmod eu consequat. Nulla mollit fugiat qui
+        incididunt aliquip aliqua eu occaecat ea et incididunt. Non ex est
+        consequat do aliqua aliqua ipsum aute commodo eu consequat. Commodo
+        dolor aliqua nostrud qui aliquip tempor magna nulla non voluptate
+        officia eiusmod. Ex Lorem mollit officia labore ut nisi occaecat anim
+        cillum eiusmod mollit et laboris sint.
+      </div>
+    </div>
+  );
 }
