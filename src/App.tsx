5cf94c490b4c43f0f65beeb0595e29a9ae6ed6a6
zawette <zawette@gmail.com>
2020-07-26 16:37:58 +0100

    styling n refract

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import Collapse from './collapse/Collapse';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Collapse from "./collapse/Collapse";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Collapse>
-      
+        Proident aute elit eiusmod eu consequat. Nulla mollit fugiat qui
+        incididunt aliquip aliqua eu occaecat ea et incididunt. Non ex est
+        consequat do aliqua aliqua ipsum aute commodo eu consequat. Commodo
+        dolor aliqua nostrud qui aliquip tempor magna nulla non voluptate
+        officia eiusmod. Ex Lorem mollit officia labore ut nisi occaecat anim
+        cillum eiusmod mollit et laboris sint.
       </Collapse>
     </div>
   );
--- src/collapse/Collapse.tsx
+++ src/collapse/Collapse.tsx
@@ -1,11 +1,13 @@
 import React, { ReactElement, useState, useRef, useLayoutEffect } from "react";
 import styles from "./Collapse.module.scss";
 
-interface Props {}
+interface Props {
+    children: React.ReactNode
+}
 
-export default function Collapse({}: Props): ReactElement {
-  // todo: contentHeight no need for useState; btn styling
-  const [expand, setexpand] = useState(true);
+export default function Collapse(props: Props): ReactElement {
+  // todo: contentHeight no need for useState;
+  const [isExpanded, setisExpanded] = useState(true);
   const [contentHeight, setcontentHeight] = useState(0);
   const contentRef = useRef<HTMLDivElement>(null);
   const setContentDivHeight = (height: number) => {
@@ -14,32 +16,27 @@ export default function Collapse({}: Props): ReactElement {
   };
   const tempFunc = (ex: boolean) => {
     ex ? setContentDivHeight(contentHeight) : setContentDivHeight(0);
-    setexpand(ex);
+    setisExpanded(ex);
     console.log(contentRef);
   };
 
   useLayoutEffect(() => {
     setcontentHeight(contentRef.current?.clientHeight || 0);
     setContentDivHeight(0);
-    return setexpand(false);
+    return setisExpanded(false);
   }, []);
 
   return (
     <div className={styles.zawCollapse}>
       <div className="top">
         temp stuff
-        <button className="expandBtn" onClick={() => tempFunc(!expand)}>
-          ^
+        <button className="expandBtn" onClick={() => tempFunc(!isExpanded)}>
+          {isExpanded ?  "▲" : "▼" }
         </button>
       </div>
 
       <div className="content" ref={contentRef}>
-        Proident aute elit eiusmod eu consequat. Nulla mollit fugiat qui
-        incididunt aliquip aliqua eu occaecat ea et incididunt. Non ex est
-        consequat do aliqua aliqua ipsum aute commodo eu consequat. Commodo
-        dolor aliqua nostrud qui aliquip tempor magna nulla non voluptate
-        officia eiusmod. Ex Lorem mollit officia labore ut nisi occaecat anim
-        cillum eiusmod mollit et laboris sint.
+        {props.children}
       </div>
     </div>
   );
