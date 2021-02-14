d8208c5a24230cc077a95e75b3b5a880d2cf572b
zawette <zawette@gmail.com>
2021-02-14 17:44:54 +0100

    f

--- example/index.tsx
+++ example/index.tsx
@@ -6,7 +6,30 @@ import Collapse from '../.';
 const App = () => {
   return (
     <div>
-      <Collapse />
+      <Collapse header="test" className="myAccordion">
+        Proident aute elit eiusmod eu consequat. Nulla mollit fugiat qui
+        incididunt aliquip aliqua eu occaecat ea et incididunt. Non ex est
+        consequat do aliqua aliqua ipsum aute commodo eu consequat. Commodo
+        dolor aliqua nostrud qui aliquip tempor magna nulla non voluptate
+        officia eiusmod. Ex Lorem mollit officia labore ut nisi occaecat anim
+        cillum eiusmod mollit et laboris sint.
+        <Collapse header="test" className="myAccordion">
+          Proident aute elit eiusmod eu consequat. Nulla mollit fugiat qui
+          incididunt aliquip aliqua eu occaecat ea et incididunt. Non ex est
+          consequat do aliqua aliqua ipsum aute commodo eu consequat. Commodo
+          dolor aliqua nostrud qui aliquip tempor magna nulla non voluptate
+          officia eiusmod. Ex Lorem mollit officia labore ut nisi occaecat anim
+          cillum eiusmod mollit et laboris sint.
+          <Collapse header="test" className="myAccordion">
+            Proident aute elit eiusmod eu consequat. Nulla mollit fugiat qui
+            incididunt aliquip aliqua eu occaecat ea et incididunt. Non ex est
+            consequat do aliqua aliqua ipsum aute commodo eu consequat. Commodo
+            dolor aliqua nostrud qui aliquip tempor magna nulla non voluptate
+            officia eiusmod. Ex Lorem mollit officia labore ut nisi occaecat
+            anim cillum eiusmod mollit et laboris sint.
+          </Collapse>
+        </Collapse>
+      </Collapse>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { useRef, useState } from 'react';
+import styles from './Collapse.module.css';
 
 interface Props {
   children: React.ReactNode;
   header: React.ReactNode;
-  className: string;
+  duration?: number;
+  className?: string;
   Key?: string;
-  initExpanded: boolean;
+  initExpanded?: boolean;
   onToggle?: (prevState: boolean) => any;
 }
 
@@ -15,24 +17,49 @@ interface RefType {
 }
 
 const Collapse = (props: Props, ref: any) => {
-  const [isExpanded, setIsExpanded] = useState(true);
+  const [isExpanded, setIsExpanded] = useState(false);
   const contentRef = useRef<HTMLDivElement>(null);
   let currentRef = props.Key && ref.current[props.Key];
-  const toggle = () => {};
+
+  const toggle = () => {
+    contentRef.current!.style.display = 'block';
+    contentRef.current!.style.transitionDuration = props.duration + 'ms';
+    const contentHeight = contentRef.current?.offsetHeight;
+    const contentPadding = contentRef.current?.style.padding;
+    if (!isExpanded) {
+      setIsExpanded(true);
+      contentRef.current!.style.height = 0 + 'px';
+      setTimeout(() => {
+        contentRef.current!.style.height = contentHeight + 'px';
+      }, 30);
+      setTimeout(() => {
+        contentRef.current!.style.height = 'auto';
+      }, 30 + props.duration!);
+    } else {
+      setIsExpanded(false);
+      contentRef.current!.style.height = 0 + 'px';
+      contentRef.current!.style.padding = 0 + 'px';
+      setTimeout(() => {
+        contentRef.current!.style.display = 'none';
+        contentRef.current!.style.height = 'auto';
+        contentRef.current!.style.padding = contentPadding!;
+      }, props.duration!);
+    }
+  };
 
   return (
     <div
-      className={`${props.className} zawCollapse`}
+      className={`${props.className} ${styles.zawCollapse}`}
       ref={element => (currentRef = element)}
     >
-      <div className="zawCollapse-header" onClick={() => toggle()}>
+      <div className={styles.zawCollapse_header} onClick={() => toggle()}>
         {props.header}
-        <button className="zawCollapse-expandBtn">
+        <button className={styles.zawCollapse_header_expandBtn}>
           {isExpanded ? '▲' : '▼'}
         </button>
       </div>
 
-      <div className="zawCollapse-content" ref={contentRef}>
+      <div className={styles.zawCollapse_content} ref={contentRef}>
         {props.children}
       </div>
     </div>
@@ -41,6 +68,7 @@ const Collapse = (props: Props, ref: any) => {
 const CollapseComponent = React.forwardRef<RefType, Props>(Collapse);
 CollapseComponent.defaultProps = {
   className: '',
+  duration: 800,
   initExpanded: false,
 } as Partial<Props>;
 
