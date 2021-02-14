431d352357f7ac63a52c163aa99dd44713f7a5dd
zawette <zawette@gmail.com>
2021-02-14 02:18:09 +0100

    init rewrite

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,47 @@
 import * as React from 'react';
+import { useRef, useState } from 'react';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+interface Props {
+  children: React.ReactNode;
+  header: React.ReactNode;
+  className: string;
+  Key?: string;
+  initExpanded: boolean;
+  onToggle?: (prevState: boolean) => any;
+}
+
+interface RefType {
+  [key: string]: React.ReactNode;
+}
+
+const Collapse = (props: Props, ref: any) => {
+  const [isExpanded, setIsExpanded] = useState(true);
+  const contentRef = useRef<HTMLDivElement>(null);
+  let currentRef = props.Key && ref.current[props.Key];
+  const toggle = () => {};
+
+  return (
+    <div
+      className={`${props.className} zawCollapse`}
+      ref={element => (currentRef = element)}
+    >
+      <div className="zawCollapse-header" onClick={() => toggle()}>
+        {props.header}
+        <button className="zawCollapse-expandBtn">
+          {isExpanded ? '▲' : '▼'}
+        </button>
+      </div>
+
+      <div className="zawCollapse-content" ref={contentRef}>
+        {props.children}
+      </div>
+    </div>
+  );
 };
+const CollapseComponent = React.forwardRef<RefType, Props>(Collapse);
+CollapseComponent.defaultProps = {
+  className: '',
+  initExpanded: false,
+} as Partial<Props>;
+
+export default CollapseComponent;
