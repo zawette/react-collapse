f502764b120c308b9e2d1a5d3aaaf6496b62095a
zawette <zawette@gmail.com>
2021-02-23 17:12:02 +0100

    small css & example modif

--- example/index.tsx
+++ example/index.tsx
@@ -7,9 +7,9 @@ const App = () => {
   const [isExpanded, setIsExpanded] = React.useState(false);
 
   return (
-    <div>
+    <div style={{ backgroundColor: 'greenyellow' }}>
       <Collapse
-        header="nested collapse"
+        header="a collapse"
         className="myAccordion"
         initExpanded
         // disabled
@@ -18,40 +18,20 @@ const App = () => {
         // setIsExpanded(!isExpanded);
         // }}
       >
-        <Collapse header="test" className="myAccordion" duration={400}>
-          Proident aute elit eiusmod eu consequat. Nulla mollit fugiat qui
-          incididunt aliquip aliqua eu occaecat ea et incididunt. Non ex est
-          consequat do aliqua aliqua ipsum aute commodo eu consequat. Commodo
-          dolor aliqua nostrud qui aliquip tempor magna nulla non voluptate
-          officia eiusmod. Ex Lorem mollit officia labore ut nisi occaecat anim
-          cillum eiusmod mollit et laboris sint. Lorem ipsum dolor sit amet,
-          consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-          labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos
-          et accusam et justo duo dolores et ea rebum. Stet clita kasd
-          gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet.
-        </Collapse>
-        <Collapse header="test" className="myAccordion" duration={200}>
-          Proident aute elit eiusmod eu consequat. Nulla mollit fugiat qui
-          incididunt aliquip aliqua eu occaecat ea et incididunt. Non ex est
-          consequat do aliqua aliqua ipsum aute commodo eu consequat. Commodo
-          dolor aliqua nostrud qui aliquip tempor magna nulla non voluptate
-          officia eiusmod. Ex Lorem mollit officia labore ut nisi occaecat anim
-          cillum eiusmod mollit et laboris sint. Lorem ipsum dolor sit amet,
-          consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-          labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos
-          et accusam et justo duo dolores et ea rebum. Stet clita kasd
-          gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet.
-        </Collapse>
+        Proident aute elit eiusmod eu consequat. Nulla mollit fugiat qui
+        incididunt aliquip aliqua eu occaecat ea et incididunt. Non ex est
+        consequat do aliqua aliqua ipsum aute commodo eu consequat. Commodo
+        dolor aliqua nostrud qui aliquip tempor magna nulla non voluptate
+        officia eiusmod. Ex Lorem mollit officia labore ut nisi occaecat anim
+        cillum eiusmod mollit et laboris sint. Lorem ipsum dolor sit amet,
+        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+        accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+        sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor
+        sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
       </Collapse>
     </div>
   );
